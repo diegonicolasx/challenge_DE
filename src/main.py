940e913b1b3b90834d2433dfd78c0704bf0ec4f1
diegonicolasx <diegonicolasx@users.noreply.github.com>
2024-10-03 20:15:01 -0300

    Upgrade in feature q1-memory

--- src/main.py
+++ src/main.py
@@ -2,8 +2,4 @@ from  q1_memory import q1_memory
 
 file_path = "data/farmers-protest-tweets-2021-2-4.json"
 
-top_ten_data = q1_memory(file_path)
-
-print("Top Ten Dates and Their Most Active Users:")
-for date, username in top_ten_data:
-    print(f"Date: {date}, Most Active User: {username}")
+print(q1_memory(file_path))
