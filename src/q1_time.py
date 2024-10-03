86e4b84ed0a553a32a4269d3736db709f2129fec
diegonicolasx <diegonicolasx@users.noreply.github.com>
2024-10-03 20:22:25 -0300

    Upgrade to the q1-time feature

--- src/q1_time.py
+++ src/q1_time.py
@@ -14,6 +14,18 @@ def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
     La función busca reconocer cada línea del archivo json y eliminar los espacios
     en blanco entre cada objeto json del archivo, para que puedan ser cargados
     mediante el metodo loads().
+    Antes de realizar la carga de datos del archivo json, se establece un contador en formato
+    diccionario llamado date_user_count, el cual permitirá contar las veces de ocurrencia de una
+    fecha junto a los usuarios, de esta manera se busca obtener el objetivo de la consulta.
+    A medida que se va cargando cada objeto json del archivo json, se irá interando
+    por sus claves o keys para encontrar las clave que son de "date" y las que son de "user".
+    De estas claves se pueden extraer los valores de fecha y de nombre de usuario.
+    Cada vez que se repita una fecha junto a un nombre de usuario, se irá agregando un 1
+    al contador, de esta manera despues se pueden obtener las fechas mas repetidas junto
+    al usuario que mas activo estuvo ese día.
+    Al final, se ordenará el diccionario que se creo de acuerdo a la fecha mas repetida
+    y se irán almacenando las fechas más repetidas junto al usuario más activo en formato tupla
+    dentro de una lista que será el resultado que retorna la función.
     """
     with open(file_path, 'rb') as file:
         date_user_count = defaultdict(Counter)
