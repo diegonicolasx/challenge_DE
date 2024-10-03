d5f07270ed336d44dabb6c3e81284755d3ba3bd8
diegonicolasx <diegonicolasx@users.noreply.github.com>
2024-10-03 20:29:06 -0300

    Modify the main.py file

--- src/main.py
+++ src/main.py
@@ -2,8 +2,4 @@ from q1_time import q1_time
 
 file_path = "data/farmers-protest-tweets-2021-2-4.json"
 
-top_ten_data = q1_time(file_path)
-
-print("Top Ten Dates and Their Most Active Users:")
-for date, username in top_ten_data:
-    print(f"Date: {date}, Most Active User: {username}")
+print(q1_time(file_path))
